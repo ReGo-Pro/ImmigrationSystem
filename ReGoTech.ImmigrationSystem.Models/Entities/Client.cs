fdef596d39cb84243693b3d5e8e52e593149f86a
Reza <r.ghochkhani@gmail.com>
2024-03-09 23:16:41 -0500

    Add client and its bare minimum properties

---
+++ ReGoTech.ImmigrationSystem.Models/Entities/Client.cs
@@ -0,0 +1,15 @@
+﻿using ReGoTech.ImmigrationSystem.Constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReGoTech.ImmigrationSystem.Models.Entities
+{
+	public class Client : Person
+	{
+        public string Uid { get; set; }
+        public ClientType Type { get; set; }
+    }
+}
