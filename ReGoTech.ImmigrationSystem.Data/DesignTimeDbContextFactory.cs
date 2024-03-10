753ed25aa0c3a1185b975f6f7fb7df58d2f2e407
Reza <r.ghochkhani@gmail.com>
2024-03-10 13:23:31 -0400

    Add DbContext and design time factory

---
+++ ReGoTech.ImmigrationSystem.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReGoTech.ImmigrationSystem.Data
+{
+	internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+	{
+		public AppDbContext CreateDbContext(string[] args) {
+			var designDbConnex = "User ID=root;Password=123456;Host=localhost;Port=5432;Database=CoreGres;";
+			var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+			optionsBuilder.UseSqlServer(designDbConnex);
+			return new AppDbContext(optionsBuilder.Options);
+		}
+	}
+}
