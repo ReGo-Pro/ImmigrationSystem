25a4608fbfbc36702b61c0b960ad94b1af7d9d17
Reza <r.ghochkhani@gmail.com>
2024-03-09 23:12:33 -0500

    Add Constants project and ClientType enum

---
+++ ReGoTech.ImmigrationSystem.Constants/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReGoTech.ImmigrationSystem.Constants
+{
+	public enum ClientType {
+		PrincipalApplicant = 1,
+		Dependent = 2
+	}
+}
