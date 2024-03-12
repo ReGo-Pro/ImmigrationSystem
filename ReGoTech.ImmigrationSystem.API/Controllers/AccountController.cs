ed43b9069a081cf38ceeb68bf0c76399cb44c3e4
Reza <r.ghochkhani@gmail.com>
2024-03-12 01:30:45 -0400

    Add AccountController with a dummy endpoint

---
+++ ReGoTech.ImmigrationSystem.API/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReGoTech.ImmigrationSystem.API.Controllers
+{
+	public class AccountController : ApiController
+	{
+		[HttpGet]
+		public async Task<IActionResult> Get() {
+			return Ok("Hello world");
+		}
+		// Add client (anonymous sign up)
+		// Login (anonymous)
+
+		// Remove client (should be authenticated - just for same client - and admin)
+		// Update client (should be authenticated - just for same client - and admin)
+		// GetClient (should be authenticated - just for same client - and admin)
+		// GetAListOfClients (for admin only)
+		// Forgot password functionality
+
+	}
+}
---
+++ ReGoTech.ImmigrationSystem.API/Controllers/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReGoTech.ImmigrationSystem.API.Controllers
+{
+	[Route("api/v1/[controller]")]
+	[ApiController]
+
+	public class ApiController : ControllerBase
+	{
+		public ApiController() { }
+	}
+}
