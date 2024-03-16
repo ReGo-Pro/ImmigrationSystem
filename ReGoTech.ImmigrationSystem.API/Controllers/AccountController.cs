3b5ce9588c53241597adf5c3c04528d01ef8ecb9
Reza <r.ghochkhani@gmail.com>
2024-03-15 20:41:46 -0400

    Add SignUp method in account controller and use validator

--- ReGoTech.ImmigrationSystem.API/Controllers/AccountController.cs
+++ ReGoTech.ImmigrationSystem.API/Controllers/AccountController.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ReGoTech.ImmigrationSystem.Models.DataTransferObjects.Inbound;
+using ReGoTech.ImmigrationSystem.Models.Entities;
+using ReGoTech.ImmigrationSystem.Services.DtoValidation;
 
 namespace ReGoTech.ImmigrationSystem.API.Controllers
 {
 	public class AccountController : ApiController
 	{
+		private IDtoValidator<ClientDtoIn> _clientDtoValidator;
+		public AccountController(IDtoValidator<ClientDtoIn> clientDtoValidator) {
+			_clientDtoValidator = clientDtoValidator;
+		}
+
 		[HttpGet]
 		public async Task<IActionResult> Get() {
 			return Ok("Hello world");
 		}
+
+		[HttpPost("SignUp")]
+		[AllowAnonymous]
+		public async Task<IActionResult> SignUp(ClientDtoIn dto) {
+			if (!ModelState.IsValid) {
+				// Should we return ModelState from here? 
+				return BadRequest(ModelState);
+			}
+
+			_clientDtoValidator.Validate(dto);
+			if (_clientDtoValidator.HasError) {
+				return BadRequest(_clientDtoValidator.ValidationErrors);
+			}
+
+			// TODO: Create client in database
+			return Ok();
+		}
+
+
 		// Add client (anonymous sign up)
 		// Login (anonymous)
 
