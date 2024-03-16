23a7884272eaebdf8c7c4e49108978df892f94c4
Reza <r.ghochkhani@gmail.com>
2024-03-15 20:40:24 -0400

    Register validator for ClientDtoIn in DI container

--- ReGoTech.ImmigrationSystem.API/Program.cs
+++ ReGoTech.ImmigrationSystem.API/Program.cs
@@ -1,3 +1,6 @@
+using ReGoTech.ImmigrationSystem.Models.DataTransferObjects.Inbound;
+using ReGoTech.ImmigrationSystem.Services.DtoValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IDtoValidator<ClientDtoIn>, ClientDtoValidator>();
 
 var app = builder.Build();
 
