c91219b00516bf2cf3d2276a0f286577f550e0d7
Reza <r.ghochkhani@gmail.com>
2024-03-16 00:35:02 -0400

    Add a generic repository and its implementation

---
+++ ReGoTech.ImmigrationSystem.Data.EntityFramework/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReGoTech.ImmigrationSystem.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReGoTech.ImmigrationSystem.Data.EntityFramework.Repositories
+{
+	public class Repository<TEntity> : IRepository<TEntity, int> where TEntity : EntityBase
+	{
+		private DbContext _dbContext;
+
+		public Repository(DbContext dbContext) {
+			_dbContext = dbContext;
+		}
+
+		public async Task<TEntity> GetByIdAsync(int id) {
+			return await _dbContext.FindAsync<TEntity>(id);
+		}
+		public async Task<List<TEntity>> GetAllAsync() {
+			return await _dbContext.FindAsync<List<TEntity>>();
+		}
+		public async Task<List<TEntity>> GetAllAsync(Func<TEntity, bool> predicate) {
+			return await _dbContext.FindAsync<List<TEntity>>(predicate);
+		}
+
+		public void Add(TEntity entity) {
+			_dbContext.Add(entity);
+		}
+		public void AddRange(IEnumerable<TEntity> entities) {
+			_dbContext.AddRange(entities);
+		}
+
+		public void Remove(TEntity entity) {
+			_dbContext.Remove(entity);
+		}
+		public void RemoveRange(IEnumerable<TEntity> entities) {
+			_dbContext.RemoveRange(entities);
+		}
+	}
+}
---
+++ ReGoTech.ImmigrationSystem.Data/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReGoTech.ImmigrationSystem.Data
+{
+	public interface IRepository<TEntity, TKey> where TEntity : class where TKey : IEquatable<TKey>
+	{
+		Task<TEntity> GetByIdAsync(TKey id);
+		Task<List<TEntity>> GetAllAsync(Func<TEntity, bool> predicate);
+		Task<List<TEntity>> GetAllAsync();
+
+		void Add(TEntity entity);
+		void AddRange(IEnumerable<TEntity> entities);
+
+		void Remove(TEntity entity);
+		void RemoveRange(IEnumerable<TEntity> entities);
+	}
+}
