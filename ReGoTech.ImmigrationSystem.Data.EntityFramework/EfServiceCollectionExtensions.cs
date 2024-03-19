ef5776445aa7e009d0a0cf5b2c62440d4d52fe2a
Reza <r.ghochkhani@gmail.com>
2024-03-18 22:34:46 -0400

    Enable lazy loading proxies for EF Core

--- ReGoTech.ImmigrationSystem.Data.EntityFramework/EfServiceCollectionExtensions.cs
+++ ReGoTech.ImmigrationSystem.Data.EntityFramework/EfServiceCollectionExtensions.cs
@@ -12,6 +12,7 @@ namespace ReGoTech.ImmigrationSystem.Data.EntityFramework
 	{
 		public static void AddEFWithSqlServer(this IServiceCollection services, string connectionString) {
 			services.AddDbContext<AppDbContext>(o => {
+				o.UseLazyLoadingProxies();
 				o.UseSqlServer(connectionString);
 			});
 		}
