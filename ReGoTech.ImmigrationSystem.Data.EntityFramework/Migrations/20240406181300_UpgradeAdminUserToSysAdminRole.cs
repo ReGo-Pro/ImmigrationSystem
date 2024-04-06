4e13831e65480604d5ab4812b9e5e35ba2b7ab88
Reza <r.ghochkhani@gmail.com>
2024-04-06 14:15:49 -0400

    Update default admin user's role to SysAdmin

---
+++ ReGoTech.ImmigrationSystem.Data.EntityFramework/Migrations/20240406181300_UpgradeAdminUserToSysAdminRole.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReGoTech.ImmigrationSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpgradeAdminUserToSysAdminRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var adminUpdate = "UPDATE Clients SET RoleId = 1 WHERE ClientId = 1";
+            migrationBuilder.Sql(adminUpdate);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+			var adminUpdate = "UPDATE Clients SET RoleId = 5 WHERE ClientId = 1"; // least privileged role
+			migrationBuilder.Sql(adminUpdate);
+		}
+    }
+}
