154f5b352096ac54cc78c8d97c119cc959cc1584
Reza <r.ghochkhani@gmail.com>
2024-04-06 14:06:29 -0400

    Add default roles using a migration

---
+++ ReGoTech.ImmigrationSystem.Data.EntityFramework/Migrations/20240406180437_AddDefaultRoles.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReGoTech.ImmigrationSystem.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			var rolesInsert = @"
+SET IDENTITY_INSERT Roles ON;
+INSERT INTO Roles (RoleId, Name, Description) VALUES
+(1, 'SysAdmin', 'Has full control over the application including system level configurations, officer and client information'),
+(2, 'TechAdmin', 'Has full control over system settings but not officer or client information'),
+(3, 'Supervisor', 'Has full control over officer and cleint information but not system settings'),
+(4, 'Officer', 'Can view client data, approve/disapprove applications but cannot modify any client information'),
+(5, 'Applicant', 'Can view and modify his/her own information including documents and profile')
+SET IDENTITY_INSERT Roles OFF;
+";
+			migrationBuilder.Sql(rolesInsert);
+		}
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
